7e5e603d617cdfbfa962a01ca5c24cacb4c7b370
Muhammad Junaid <m.junaidaqib@gmail.com>
2024-02-29 01:03:35 +0500

    authentication is done

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const path = require("path");
 const mongoose = require("mongoose");
+const cookieParser = require('cookie-parser')
 
-const userRouter = require('./routes/routes')
+const userRouter = require('./routes/routes');
+const { checkForAuthenticationCookie } = require("./middlewares/authenticationMiddleware");
 
 const app = express();
 const PORT = 5000;
@@ -13,10 +15,14 @@ app.set("view engine", "ejs");
 app.set('views', path.resolve('./views'))
 
 app.use(express.urlencoded({ extended: false}))
+app.use(cookieParser())
+app.use(checkForAuthenticationCookie('token'))
 
 app.get("/", (req, res) => {
     //   return res.send("Welcome to Home page!");
-    return res.render("home")
+    return res.render("home",{
+      user: req.user
+    })
 });
 
 app.use('/user', userRouter)
